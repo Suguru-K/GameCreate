6c45872dfd32003897e185b4eacc7bccc3caa517
Suguru-K <lionheart.sgr@gmail.com>
2019-05-03 15:11:15 +0900

    title scene updated.

---
+++ MyBallGame/Assets/startScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startScript : MonoBehaviour
+{
+    // 初期処理
+    void Start()
+    {
+        
+    }
+
+    // フレーム毎処理
+    void Update()
+    {
+        
+    }
+}
