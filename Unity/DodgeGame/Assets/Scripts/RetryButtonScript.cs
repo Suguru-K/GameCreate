ff20896b9f0580d24efe25169f384b693818b3cc
Suguru-K <lionheart.sgr@gmail.com>
2019-05-07 13:00:22 +0900

    GameOverScene updated

--- Unity/DodgeGame/Assets/Scripts/RetryButtonScript.cs
+++ Unity/DodgeGame/Assets/Scripts/RetryButtonScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonScript : MonoBehaviour
+public class RetryButtonScript : MonoBehaviour
 {
     public void ReplayGame2()
     {
---
+++ Unity/DodgeGame/Assets/Scripts/TitleButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButtonScript : MonoBehaviour
+{
+    public void ToTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    // 初期処理
+    void Start()
+    {
+        
+    }
+
+    // フレーム毎処理
+    void Update()
+    {
+        
+    }
+}
