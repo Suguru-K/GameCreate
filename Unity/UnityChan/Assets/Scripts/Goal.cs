d7106cc789293e2b054ccb0df77488b2cd7f5b9a
Suguru-K <lionheart.sgr@gmail.com>
2020-02-29 12:41:19 +0900

    upload Unitychan

---
+++ Unity/UnityChan/Assets/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //ゴールした場合
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("GOAL!");
+        }
+    }
+}
---
+++ Unity/UnityChan/Assets/Scripts/RotateObjScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObjScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0.5f, 0));
+    }
+}
